bed165bea1d54d5cc6b6530abbfbd08cece4e611
xdyy99 <65595915+xdyy99@users.noreply.github.com>
2021-03-12 17:02:56 +0700

    update Century2

--- CenturyCity/js/index.js
+++ CenturyCity/js/index.js
@@ -96,7 +96,36 @@ $('.news__slide').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
+    autoplay: true,
     responsive: [
+		{
+		  breakpoint: 800,
+		  settings: {
+			slidesToShow: 2,
+		  }
+		},
+		{
+		  breakpoint: 600,
+		  settings: {
+			slidesToShow: 1,
+		  }
+		}
+	]
+});
+////////////// slick update //////////////
+$('.update__slide').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    responsive: [
+        {
+            breakpoint: 1750,
+            settings: {
+              slidesToShow: 3,
+            }
+          },
 		{
 		  breakpoint: 800,
 		  settings: {
