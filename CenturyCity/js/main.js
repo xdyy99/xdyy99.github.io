e22f87870d7e9d130d7b4cffc173118d7726a1ce
xdyy99 <65595915+xdyy99@users.noreply.github.com>
2021-03-12 15:59:37 +0700

    update Century

---
+++ CenturyCity/js/main.js
@@ -0,0 +1,53 @@
+////////////// menu control //////////////
+var menuBtn = document.querySelector(".menuBtn");
+var menu = document.querySelector(".header__bottom");
+
+
+menuBtn.addEventListener("click", menuAct);
+menu.addEventListener("click", menuAct);
+function menuAct(){
+    menuBtn.classList.toggle("active");
+    menu.classList.toggle("active");   
+}
+
+
+
+////////////// back to top //////////////
+function  backToTop(){
+    window.scrollTo({
+      top: 0,
+    })
+  }
+
+////////////// scroll //////////////
+
+window.addEventListener("scroll", function () {
+
+    // back to top
+    var toTop = document.querySelector(".backToTop");
+    if(window.scrollY > 400) toTop.classList.add("active");
+    else toTop.classList.remove("active");
+
+    //header
+    var header = document.querySelector(".header");
+    if(window.scrollY > 100) header.classList.add("header__small");
+    else header.classList.remove("header__small");
+
+});
+
+////////////// footer form //////////////
+var formInp = document.querySelectorAll(".formInput");
+var formLine = document.querySelectorAll(".formLine");
+
+for(let i = 0; i < formInp.length; i++){
+    formInp[i].addEventListener("click", showLine);
+    function showLine() {
+        for(let a = 0; a < formInp.length; a++){
+            formInp[a].classList.remove("active");
+            formLine[a].classList.remove("active");
+        }
+        formInp[i].classList.add("active");
+        formLine[i].classList.add("active");
+    }
+}
+
