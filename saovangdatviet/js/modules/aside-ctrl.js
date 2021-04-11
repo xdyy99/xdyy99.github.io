9962c5db835cc9fcdcb2f0e3dce0e28c37356e6e
xdyy99 <65595915+xdyy99@users.noreply.github.com>
2021-04-12 01:09:49 +0700

    a
    as

--- saovangdatviet/js/modules/aside-ctrl.js
+++ saovangdatviet/js/modules/aside-ctrl.js
@@ -1,18 +1,20 @@
 export default function colBtn() {
     var colBtn = document.querySelector(".col__control");
     var col = document.querySelector(".aside__col");
-    
-    if(colBtn){
+    var colBg = document.querySelector(".col__bg");
+
+    if (colBtn) {
         window.addEventListener("scroll", function () {
-        if(window.scrollY > 100) {
-            colBtn.classList.add("show");
-        } 
-        else colBtn.classList.remove("show");
-        })
+            if (window.scrollY > 100) {
+                colBtn.classList.add("show");
+            } else colBtn.classList.remove("show");
+        });
         colBtn.addEventListener("click", colAct);
-        function colAct(){
+        colBg.addEventListener("click", colAct);
+        function colAct() {
             colBtn.classList.toggle("active");
-            col.classList.toggle("active");  
+            col.classList.toggle("active");
+            colBg.classList.toggle("active");
         }
     }
 }
