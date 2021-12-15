b1ce45205af6a120e1c78f62888280d51ecd72f9
xdyy99 <AT141016@actvn.edu.vn>
2021-12-15 12:40:50 +0700

    Final
    Final update

--- ProjectMona/simlock/template/getfile.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $dir    = '.';
-    
-    $files1 = glob("*.html");
-    foreach ($files1 as $value) {
-        echo '<p><a href="'. $value .'" target="_blank">'.$value.'</a></p>';
-    }
-    
-?>
